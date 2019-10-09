782da1dccdf88039153a234d3ef34d97ba0a9d33
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-09 13:16:59 +0100

    copying master

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
+      { id: 4, name: 'Rear spoiler', price: 250 } 
     ]
   };
 
